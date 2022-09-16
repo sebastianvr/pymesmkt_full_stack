7cac291f28db32150835c54dadac98b6c17f8ebc
Sebastian Vidal <sebastianvr63@gmail.com>
2022-09-16 03:05:36 -0300

    importo locale para usar pipes segun CL

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Cambiar el locale de la app
+import localeEsCL from '@angular/common/locales/es-CL';
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(localeEsCL)
 
 @NgModule({
   declarations: [
@@ -16,7 +21,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {provide : LOCALE_ID, useValue : 'es-CL'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
