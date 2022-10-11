67bd83dab3df0fb5be1b36a9e47cc1f661becc72
Sebastian Vidal <sebastianvr63@gmail.com>
2022-10-11 02:44:31 -0300

    creacion del servicio para pagos

---
+++ frontend/src/app/core/services/pago/pago.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PagoService {
+
+  private url: string = environment.baseUrl
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getTransaccion(data: any): Observable<any> {
+    return this.http.get(`${this.url}/api/pago/create?amount=${data.amount}&returnUrl=${data.address}`)
+  }
+
+  getCommitPago(token: any): Observable<any> {
+    return this.http.get(`${this.url}/api/pago/commit?token_ws=${token}`)
+  }
+} 
