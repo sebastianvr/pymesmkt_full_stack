dda27cbd1f3fa0d978b6b1cd590c9a6980d014e4
Sebastian Vidal <sebastianvr63@gmail.com>
2022-10-17 04:09:07 -0300

    creando vista de compras, añadiendo servicio

--- frontend/src/app/user/pages/purchases/purchases.component.ts
+++ frontend/src/app/user/pages/purchases/purchases.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PublicacionService } from '../../../core/services/publicacion/publicacion.service';
+import { AuthService } from '../../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-purchases',
@@ -7,11 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PurchasesComponent implements OnInit {
 
+  usuario: any
 
-  cards : any
-  constructor() { }
+  cards: any
+  constructor(
+    private publicacionService: PublicacionService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    console.log(this.authService.usuario.id)
+    this.publicacionService.getPublicacionesPagadasById(this.authService.usuario.id, 0, 10)
+      .subscribe((res) => {
+        this.cards = res.content
+        console.log('cards', this.cards)
+        console.log('res: ', res)
+      })
   }
 
 }
