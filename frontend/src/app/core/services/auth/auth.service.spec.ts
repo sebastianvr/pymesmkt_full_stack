d4c492bd4b6fb0605e29b6366df5a17520bbbf63
Sebastian Vidal <sebastianvr63@gmail.com>
2022-12-12 04:09:48 -0300

    añadiendo prenas para authenticacion

--- frontend/src/app/core/services/auth/auth.service.spec.ts
+++ frontend/src/app/core/services/auth/auth.service.spec.ts
@@ -1,21 +1,54 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports : [
-        HttpClientTestingModule
+      imports: [
+        HttpClientModule
       ]
     });
     service = TestBed.inject(AuthService);
   });
 
-  it('should be created', () => {
+  it('Debe crear el servicio', () => {
     expect(service).toBeTruthy();
   });
+
+  test('PI04 - Debe registrar un nuevo usuario', (done) => {
+    const mockNewUser: any =  {
+      nombreUsuario: "Alan",
+      apellidos: "Vidal Ramirez",
+      run: "13.234.567-34",
+      emailUsuario: "sebaea@mail.com",
+      contrasenia: "1234.asd",
+      region: "Valparaiso",
+      comuna: "San Antonio",
+      dir1Propietario: "Alfonso XIII #738, Cerro Alegre",
+      dir2Propietario: null,
+      descripcion: "Cerca de la esquina de la plaza principal"
+  }
+    service.registerUser(mockNewUser).subscribe((data) => {
+      console.log(data)
+
+      done();
+    });
+  });
+
+  test('PI02 - Debe iniciar sesión', (done) => {
+    const mockLogIn = {
+      email: "",
+      contrasenia: "",
+    }
+    service.login(mockLogIn).subscribe((data) => {
+      console.log(data)
+
+      done();
+    })
+  });
+
 });
