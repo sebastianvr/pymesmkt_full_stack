e4e8fc5d622f46fc5fc5744db83c55d6c04da675
Sebastian Vidal <sebastianvr63@gmail.com>
2022-12-12 04:10:08 -0300

    añadiendo pruebas para oferta publicacion y reclamo

--- frontend/src/app/core/services/reclamo/reclamo.service.spec.ts
+++ frontend/src/app/core/services/reclamo/reclamo.service.spec.ts
@@ -1,21 +1,95 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ReclamoService } from './reclamo.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 describe('ReclamoService', () => {
   let service: ReclamoService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports : [
-        HttpClientTestingModule
+      imports: [
+        HttpClientModule
       ]
     });
     service = TestBed.inject(ReclamoService);
   });
 
-  it('should be created', () => {
+  test('Debe crear el servicio', () => {
     expect(service).toBeTruthy();
   });
+
+  test('PIO5 - Debe crear un nuevo reclamo', (done) => {
+    
+    const nuevoReclamo = {
+      titulo: '',
+      mensaje: '',
+      documento: '',
+      PublicacionId: '',
+      // id del usuario que RECIBIRÁ el reclamo
+      UsuarioId: '',
+    }
+
+    service.postReclamo(nuevoReclamo).subscribe((data) => {
+      console.log('data', data);
+      done();
+    })
+    expect(service).toBeTruthy();
+    
+  });
+  
+  test('PIO9.1 - Debe actualizar un reclamo', (done) => {
+    
+    const nuevoReclamo = {
+      titulo: '',
+      mensaje: '',
+      documento: '',
+      PublicacionId: '',
+      // id del usuario que RECIBIRÁ el reclamo
+      UsuarioId: '',
+    }
+
+    service.postReclamo(nuevoReclamo).subscribe((data) => {
+      console.log('data', data);
+      done();
+    })
+  });
+  
+  test('PIO9.2 - Debe buscar un reclamo', (done) => {
+    
+    const nuevoReclamo = {
+      titulo: '',
+      mensaje: '',
+      documento: '',
+      PublicacionId: '',
+      // id del usuario que RECIBIRÁ el reclamo
+      UsuarioId: '',
+    }
+
+    service.postReclamo(nuevoReclamo).subscribe((data) => {
+      console.log('data', data);
+      done();
+    })
+  });
+  
+  test('PIO9.2 - Debe eliminar un reclamo', (done) => {
+    
+    const nuevoReclamo = {
+      titulo: '',
+      mensaje: '',
+      documento: '',
+      PublicacionId: '',
+      // id del usuario que RECIBIRÁ el reclamo
+      UsuarioId: '',
+    }
+
+    service.postReclamo(nuevoReclamo).subscribe((data) => {
+      console.log('data', data);
+      done();
+    })
+  });
+
+
+
 });
