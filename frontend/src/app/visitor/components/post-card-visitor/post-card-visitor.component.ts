bfe6cf935b5bc6d6b8f26620f60bce101bf91f09
Sebastian Vidal <sebastianvr63@gmail.com>
2023-07-11 23:46:14 -0400

    implementacion de paginacion para publicaciones

--- frontend/src/app/visitor/components/post-card-visitor/post-card-visitor.component.ts
+++ frontend/src/app/visitor/components/post-card-visitor/post-card-visitor.component.ts
@@ -13,11 +13,13 @@ export class PostCardVisitorComponent implements OnInit {
   empresa!: any
   idUsuario!: string
 
+  isEmptyPublicaciones : boolean = true
+  isLoading : boolean = true
 
   // Pagina actual
   page = 0;
   // Tamaño de elementos por página
-  size = 2;
+  size = 5;
   // Representa a la cantidad total de publicaciones creadas
   numElement!: number;
 
@@ -36,15 +38,32 @@ export class PostCardVisitorComponent implements OnInit {
   }
 
   getPublicaciones() {
-    this.publicacionService.getAllPublicaciones(this.page - 1, this.size)
+    this.publicacionService.getAllPublicaciones(this.page, this.size)
       .subscribe(({ content, totalPages }) => {
-        
         this.publicaciones = content;
         this.numElement = totalPages * this.size;
+        this.isEmptyPublicaciones = content.length === 0;
+        this.isLoading = false;
       })
   }
 
   done() {
     this.getPublicaciones();
+  }
+
+  selectPage(page: any) {
+    // Validar que la página sea un número válido
+    if (Number.isInteger(Number(page))) {
+      this.page = Number(page);
+      this.getPublicaciones();
+    }
+  }
+
+  formatInput(target: any) {
+    // Validar y formatear el valor ingresado en el campo de entrada de paginación
+    const value = target.value.trim();
+    if (value !== '') {
+      target.value = Math.max(1, Math.min(value, this.numElement));
+    }
   }
 }
