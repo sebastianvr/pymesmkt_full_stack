156b25c30456e54748f9c2a56dd6bf1c77b53ae4
Sebastian Vidal <sebastianvr63@gmail.com>
2023-07-11 23:46:40 -0400

    creacion de nuevo componente para filtrar publicaciones

---
+++ frontend/src/app/visitor/components/filter-publication/filter-publication.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-publication',
+  templateUrl: './filter-publication.component.html',
+  styleUrls: ['./filter-publication.component.css']
+})
+export class FilterPublicationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
