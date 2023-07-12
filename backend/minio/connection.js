afc66ca32806faf325563b34f7edb3208d81f9d3
Sebastian Vidal <sebastianvr63@gmail.com>
2023-07-12 00:01:37 -0400

    conexion desde el servicio de api a minio

---
+++ backend/minio/connection.js
@@ -0,0 +1,25 @@
+const Minio = require('minio');
+
+const minioClient = new Minio.Client({
+    endPoint: "172.19.0.3",
+    port: 9000,
+    useSSL: false,
+    accessKey: process.env.MINIO_ACCESS_KEY,
+    secretKey: process.env.MINIO_SECRET_KEY,
+});
+
+async function createBucket(bucketName) {
+    try {
+        const exists = await minioClient.bucketExists(bucketName);
+        if (!exists) {
+            await minioClient.makeBucket(bucketName);
+        }
+        console.log('Bucket creado exitosamente');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+createBucket('files-bucket');
+
+module.exports = { minioClient };
