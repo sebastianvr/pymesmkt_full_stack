f09781c4e91122c8765e8a9298077b00bbb4c584
Sebastian Vidal <sebastianvr63@gmail.com>
2023-09-03 17:02:16 -0300

    combios para busqueda de publicacion mediante un filtro, cambios en formulario reactivo.

--- frontend/src/app/visitor/components/filter-publication/filter-publication.component.ts
+++ frontend/src/app/visitor/components/filter-publication/filter-publication.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-filter-publication',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterPublicationComponent implements OnInit {
 
-  constructor() { }
+  filterForm!: FormGroup;
+
+  rangePrice : string[] = [
+    '100.000 - 500.000',
+    '500.000 - 1.000.000',
+    '1.000.000 - 5.000.000',
+    '5.000.000 - 10.000.000',
+  ]
+  
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.filterForm = this.formBuilder.group({
+      cantOfertas: [null],
+      monto: [null],
+      garantia: [false],
+      tipo: [false],
+    })
+
+  }
 
   ngOnInit(): void {
   }
 
+  sendForm() {
+
+  }
+
+  
 }
