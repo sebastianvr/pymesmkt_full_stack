3bd997120e86f24d4ea662cdf7f5777d868f5cd0
Sebastian Vidal <sebastianvr63@gmail.com>
2023-09-18 17:55:03 -0300

    refactorizacion del filtro implementacion de formularios reactivos

---
+++ frontend/src/app/user/components/filter-publication/filter-publication.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'user-filter-publication',
+  templateUrl: './filter-publication.component.html',
+  styleUrls: ['./filter-publication.component.css']
+})
+export class FilterPublicationComponent implements OnInit {
+
+  filterForm!: FormGroup;
+
+  rangePrice: string[] = [
+    '100.000 - 500.000',
+    '500.000 - 1.000.000',
+    '1.000.000 - 5.000.000',
+    '5.000.000 - 10.000.000',
+  ]
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.filterForm = this.formBuilder.group({
+      cantOfertas: [null],
+      monto: [null],
+      garantia: [false],
+      tipo: [false],
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  sendForm() {
+  }
+}
