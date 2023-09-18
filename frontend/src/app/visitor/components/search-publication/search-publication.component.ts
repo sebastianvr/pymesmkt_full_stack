e75161145ec4446111e91b94f6a28e9435520078
Sebastian Vidal <sebastianvr63@gmail.com>
2023-09-18 18:01:03 -0300

    feat : nuevos cambios para manejar busqueda y dropdown para añadir opciones de busqueda

--- frontend/src/app/visitor/components/search-publication/search-publication.component.ts
+++ frontend/src/app/visitor/components/search-publication/search-publication.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-search-publication',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-publication.component.css']
 })
 export class SearchPublicationComponent implements OnInit {
+  filterForm!: FormGroup;
+  searchOptions: string[] = ['ID', 'Título'];
+  selectedOption: string | null = null;
 
-  constructor() { }
-
+  constructor(private formBuilder: FormBuilder) {}
   ngOnInit(): void {
+    this.buildForm();
   }
 
+  buildForm() {
+    this.filterForm = this.formBuilder.group({
+      searchOption: [null],
+      searchTerm: [null],
+    });
+  }
+
+  searchPublication() {
+    if (!this.filterForm.valid) {
+      return;
+    }
+
+    const formValues = this.filterForm.value;
+
+    // Realiza la búsqueda en función de formValues.searchOption y formValues.searchTerm
+    // Puedes implementar la lógica de búsqueda aquí y mostrar los resultados
+    console.log('Opción de búsqueda:', formValues.searchOption);
+    console.log('Término de búsqueda:', formValues.searchTerm);
+  }
+
+   selectSearchOption(option: string) {
+    this.selectedOption = option;
+    this.filterForm.patchValue({ searchOption: option });
+  }
 }
