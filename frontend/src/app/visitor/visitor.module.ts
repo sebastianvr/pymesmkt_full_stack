6edc0954a6fe44d5d1c6e3a273d69ed3c22a6d00
Sebastian Vidal <sebastianvr63@gmail.com>
2023-10-10 11:40:33 -0300

    importando nuevo compoennte TermsAndConditionsComponent, implementacion con ngBootstrap

--- frontend/src/app/visitor/visitor.module.ts
+++ frontend/src/app/visitor/visitor.module.ts
@@ -14,6 +14,7 @@ import { PrimeNgModule } from '../shared/modules/prime-ng/prime-ng.module';
 
 import { NgbModule, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { FilterPublicationComponent } from './components/filter-publication/filter-publication.component';
+import { TermsAndConditionsComponent } from './components/terms-and-conditions/terms-and-conditions.component';
 
 
 const components: any[] = [
@@ -31,6 +32,7 @@ const components: any[] = [
 @NgModule({
   declarations: [
     ...components,
+    TermsAndConditionsComponent,
   ],
   imports: [
     CommonModule,
