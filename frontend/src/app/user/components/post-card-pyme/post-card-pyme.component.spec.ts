d743f3b45298e9e4ff0a687c32dcf28096628046
Sebastian Vidal <sebastianvr63@gmail.com>
2023-11-08 12:29:16 -0300

    feature : cambiando ui ux de visualizacion de una publicacion. Implementacion de modal de oferta

--- frontend/src/app/user/components/post-card-pyme/post-card-pyme.component.spec.ts
+++ frontend/src/app/user/components/post-card-pyme/post-card-pyme.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PostCardPymeComponent } from './post-card-pyme.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { OfertaService } from '../../../core/services/oferta/oferta.service';
 
