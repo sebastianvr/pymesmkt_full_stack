45fa72c760bd98574dfa586923e2b9a46c00be1a
Sebastian Vidal <sebastianvr63@gmail.com>
2023-11-19 22:43:18 -0300

    Feature: nueva busqueda de ofertas recibidas a partir de una query.

--- frontend/src/app/user/pages/offers-received/offers-received.component.ts
+++ frontend/src/app/user/pages/offers-received/offers-received.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { OfertaService } from '../../../core/services/oferta/oferta.service';
-import { AuthService } from '../../../core/services/auth/auth.service';
+import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
-import { Router, ActivatedRoute } from '@angular/router';
-import { PagoService } from '../../../core/services/pago/pago.service';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import { Subscription } from 'rxjs';
+import { OfertaService } from 'src/app/core/services/oferta/oferta.service';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-offers-received',
@@ -14,49 +12,75 @@ import { Subscription } from 'rxjs';
 })
 export class OffersReceivedComponent implements OnInit, OnDestroy {
 
-  publicaciones: any[] = [];
-  token: string = '';
-  url: string = '';
+  idUser: string = this.authService.usuario.id;
 
-  closeResult: string = ''
+  pageSize: number = 20;
+  page: number = 1;
 
-  suscription!: Subscription;
+  isLoading: boolean = false;
+  isEmptyOffersReceived: boolean = true;
+  ofertas: any;
+  total: any;
+  currentPage!: number;
+  totalPages!: number;
+
+  closeResult: string = '';
+
   constructor(
     private router: Router,
-    private activatedRoute: ActivatedRoute,
     private ofertaService: OfertaService,
     private authService: AuthService,
-    private modalService: NgbModal
+    private modalService: NgbModal,
   ) {
   }
 
   ngOnInit(): void {
-    this.getOfertas();
-    this.suscription = this.ofertaService.refresh$.subscribe(() => {
-      this.getOfertas();
-    })
+    this.getOfertasById(this.idUser);
   }
 
   ngOnDestroy(): void {
-      this.suscription.unsubscribe();
   }
 
-  getOfertas() {
-    this.ofertaService.getOfertasRecibidas(this.authService.usuario.id).subscribe((data) => {
-      this.publicaciones = data.ofertas
-      console.log('this.publicaciones : ', this.publicaciones)
-    })
+  private getOfertasById(filters: any) {
+    this.isLoading = true;
+    console.log({ filters });
+    this.ofertaService.getOfertasById(this.idUser, filters).subscribe((data) => {
+      console.log({ data });
+      this.isLoading = false;
+
+      if (data.rows.length === 0) {
+        this.isEmptyOffersReceived = true;
+      } else {
+        const {
+          rows,
+          total,
+          currentPage,
+          pageSize,
+          totalPages,
+        } = data;
+
+        this.total = total;
+        this.currentPage = currentPage;
+        this.pageSize = pageSize;
+        this.totalPages = totalPages;
+        this.ofertas = rows;
+        this.isEmptyOffersReceived = false;
+
+        console.log(this.ofertas);
+      }
+
+      this.isLoading = false;
+    });
   }
 
   eliminarOferta(idOferta: string) {
-    // console.log(idOferta)
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
         confirmButton: 'btn btn-success mx-3',
         cancelButton: 'btn btn-danger mx-3'
       },
-      buttonsStyling: false
-    })
+      buttonsStyling: false,
+    });
 
     swalWithBootstrapButtons.fire({
       title: '¿Estás seguro de rechazar esta oferta?',
@@ -66,19 +90,18 @@ export class OffersReceivedComponent implements OnInit, OnDestroy {
       showCancelButton: true,
       confirmButtonText: 'Eliminar',
       cancelButtonText: 'Cancelar',
-      reverseButtons: true
+      reverseButtons: true,
     }).then((result) => {
       if (result.isConfirmed) {
         this.ofertaService.deleteOferta(idOferta).subscribe((data) => {
-          console.log(data)
-        })
+          console.log(data);
+        });
 
         swalWithBootstrapButtons.fire(
           'Eliminado!',
           'La oferta ha sido descartada.',
-          'success'
-        )
-
+          'success',
+        );
       }
     })
   }
@@ -93,26 +116,23 @@ export class OffersReceivedComponent implements OnInit, OnDestroy {
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Ir a pagar!',
-      cancelButtonText: 'Salir'
+      cancelButtonText: 'Salir',
     }).then((result) => {
       if (result.isConfirmed) {
-        // Swal.fire(
-        //   'Deleted!',
-        //   'Your file has been deleted.',
-        //   'success'
-        // )
-
-        // 
       }
-    })
+    });
   }
 
   open(content: any) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', centered: true }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+    this.modalService.open(content,
+      {
+        ariaLabelledBy: 'modal-basic-title',
+        centered: true,
+      }).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
   }
 
   private getDismissReason(reason: any): string {
@@ -125,4 +145,15 @@ export class OffersReceivedComponent implements OnInit, OnDestroy {
     }
   }
 
+  public clearFilter() {
+    const query = {
+      page: 1,
+      pageSize: 20,
+    };
+    // this.getPublicationsById(query);
+
+    // const searchTermControl = this.filterForm.get('searchTerm');
+    // searchTermControl?.setValue(null);
+    // searchTermControl?.reset(); 
+  }
 }
