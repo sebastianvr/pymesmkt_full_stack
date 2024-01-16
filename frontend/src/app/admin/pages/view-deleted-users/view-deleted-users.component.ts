c1d2bbb30d5086f81896cbe982a76acaede54cde
Sebastian Vidal <sebastianvr63@gmail.com>
2024-01-16 12:15:10 -0300

    feat : manejando desde el front, query para ver usuarios eliminados

--- frontend/src/app/admin/pages/view-deleted-users/view-deleted-users.component.ts
+++ frontend/src/app/admin/pages/view-deleted-users/view-deleted-users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { UsuarioService } from '../../../core/services/usuario/usuario.service';
-import Swal from 'sweetalert2';
 import { Subscription } from 'rxjs';
+import { UsuarioService } from 'src/app/core/services/usuario/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-deleted-users',
@@ -12,63 +12,69 @@ export class ViewDeletedUsersComponent implements OnInit, OnDestroy {
 
   searchText: string = '';
   allUsuariosDeleted: any;
+  suscription!: Subscription;
 
-  suscription!: Subscription
+  pageSize: number = 20;
+  page: number = 1;
+  currentPage!: number;
+  total!: number;
+  totalPages!: number;
+
+  isLoading: boolean = true;
+  isEmptyUsers: boolean = false;
 
   constructor(
-    private usuarioService: UsuarioService
+    private usuarioService: UsuarioService,
   ) { }
 
   ngOnInit(): void {
-    this.getAll();
+    console.log('ngOnInit()');
+    this.getAll(this.pageSize, this.page);
 
-    this.suscription = this.usuarioService.refresh.subscribe(() => {
-      this.getAll();
-    })
+    this.suscription = this.usuarioService.refresh.subscribe((data) => {
+      console.log({ data });
+      this.getAll(this.pageSize, this.page);
+    });
+  }
+
+  getAll(pageSize: number, page: number) {
+    this.isLoading = true;
+
+    this.usuarioService.getAllUsuariosDeleted(page, pageSize).subscribe((data) => {
+      this.isLoading = false;
+      console.log({ data });
+      if (data.usuarios.length === 0) {
+        this.isEmptyUsers = true;
+      } else {
+        const {
+          usuarios,
+          total,
+          currentPage,
+          pageSize,
+          totalPages,
+        } = data;
+
+        this.allUsuariosDeleted = usuarios;
+        this.total = total;
+        this.currentPage = currentPage;
+        this.pageSize = pageSize;
+        this.totalPages = totalPages;
+
+        this.isEmptyUsers = false;
+
+
+        console.log(this.allUsuariosDeleted);
+      }
+    });
+  }
+
+  onPageChange(newPage: number) {
+    console.log({newPage});
+    this.getAll(this.pageSize, newPage);
   }
 
   ngOnDestroy(): void {
     this.suscription.unsubscribe();
     console.log('observable cerrado')
-  }
-
-
-  getAll() {
-    this.usuarioService.getAllUsuariosDeleted(0, 10).subscribe((data) => {
-      this.allUsuariosDeleted = data.content
-      console.log('this.allUsuariosDeleted', this.allUsuariosDeleted)
-    })
-  }
-
-  activarUsuario(usuario: any) {
-    const swalWithBootstrapButtons = Swal.mixin({
-      customClass: {
-        confirmButton: 'btn btn-success mx-3',
-        cancelButton: 'btn btn-danger mx-3'
-      },
-      buttonsStyling: false
-    })
-
-    swalWithBootstrapButtons.fire({
-      title: '¿Estás seguro de reintegrar este usuario?',
-      icon: 'warning',
-      iconColor: 'red',
-      showCancelButton: true,
-      confirmButtonText: 'Reintegrar',
-      cancelButtonText: 'Cancelar',
-      reverseButtons: true
-    }).then((result) => {
-      if (result.isConfirmed) {
-        swalWithBootstrapButtons.fire(
-          'Reintegrado!',
-          'Este usuario ha sido reintegrado al sistema.',
-          'success'
-        )
-        this.usuarioService.activarUsuario(usuario).subscribe((data) => {
-          console.log(data)
-        })
-        // this.router.navigate(['/user/see-publications'])
-      }
-    })
   }
 }
