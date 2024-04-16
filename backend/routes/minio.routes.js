2c51195bd5c43e1502a21dcf42ea352df55ca262
Sebastian Vidal <sebastianvr63@gmail.com>
2024-04-16 14:58:44 -0400

    feat: nueva ruta '/reportFiles' para subir archivos minio.

--- backend/routes/minio.routes.js
+++ backend/routes/minio.routes.js
@@ -4,19 +4,28 @@ const { validarCampos } = require('../middlewares/validar-campos');
 
 const { check, param, body, query } = require('express-validator');
 const { validarJWT } = require('../middlewares/validar-jwt');
-const { uploadUserImage, getProfileUserImage } = require('../controllers/minio.controller');
+const { uploadUserImage, getProfileUserImage, postReportFiles } = require('../controllers/minio.controller');
+const validateFiles = require('../middlewares/files/validate-file');
+const checkEmptyFiles = require('../middlewares/files/check-empty-files');
 
 const router = Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
-router.post('/uploadUserImage', upload.single('image'), uploadUserImage);
 
+router.post('/uploadUserImage',
+    upload.single('image'),
+    uploadUserImage
+);
 
 router.get('/profileImage', [
     query('image').notEmpty().withMessage('El parámetro image es obligatorio'),
     validarCampos,
 ], getProfileUserImage);
+
+router.post('/reportFiles', [
+    validateFiles,
+], postReportFiles);
 
 
 module.exports = router;
