99f146bb66e870070224f5295404930e05511dc7
Sebastian Vidal <sebastianvr63@gmail.com>
2024-05-01 02:46:48 -0400

    fix: Quitando logs en controlador para calificacion, cambios para mostrar al cleinte mensajes de errores del servidor.

--- backend/controllers/calificacion.controller.js
+++ backend/controllers/calificacion.controller.js
@@ -1,64 +1,58 @@
-const { uid } = require('uid');
 const { response, request } = require('express');
+const { uid } = require('uid');
 
 const Calificacion = require('../models/calificacion');
 const Compra = require('../models/compra');
 
 
 const calificacionPost = async (req = request, res = response) => {
-    //Id unico de 15 caracteres, para cada nueva publicacion creada
-    const myId = uid(15);
+    console.log('[calificaciones] calificacionPost()');
 
     const {
         reseña,
         puntaje,
-        // id del usuario que RECIBIRÁ la calificacion
-        UsuarioId,
-        // id de la compra que esta calificando
-        CompraId
-    } = req.body
+        UsuarioId, // id del usuario que RECIBIRÁ la calificacion
+        CompraId // id de la compra que esta calificando
+    } = req.body;
 
-
-    const nuevaCalificacion = {
-        id: myId,
+    const newId = uid(15);
+    const newCalification = {
+        id: newId,
         reseña,
         puntaje,
         UsuarioId,
         CompraId
-    }
+    };
 
     // console.log(nuevaCalificacion)
-
-    try { 
+    try {
         // primero creo la nueva calificacion
-        const { id } = await Calificacion.create(nuevaCalificacion);
-
+        const { id } = await Calificacion.create(newCalification);
 
         // actualizo tabla compra con el id de calificacion
-        const compra = await Compra.update({ CalificacionId: id }, {
-            where: { id: nuevaCalificacion.CompraId }
-        });
+        const compra = await Compra.update(
+            { CalificacionId: id },
+            { where: { id: newCalification.CompraId } }
+        );
 
         // console.log('compra', compra)
         return res.status(200).json({
             ok: true,
-            id,
-            msg: 'Nueva calificación creada'
-        })
-
+            msg: 'Nueva calificación creada',
+            id
+        });
 
     } catch (error) {
-        console.log(error)
-        return res.status(400).json({
+        console.error(error);
+        return res.status(500).json({
             ok: false,
-            error,
-            msg: 'Error al crear calificación.'
+            msg: 'Error en el servidor, calificacionPost().',
+            error
         })
     }
 
 }
 
-
 module.exports = {
-    calificacionPost
+    calificacionPost,
 };
