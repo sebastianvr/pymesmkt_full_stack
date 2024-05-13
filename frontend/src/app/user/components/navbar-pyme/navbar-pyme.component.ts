edd663216952236d6b26b5853ddc2e89adce715d
Sebastian Vidal <sebastianvr63@gmail.com>
2024-05-13 17:05:32 -0400

    fix: Identando correctamente el código de nav-bar, removiendo rutas a nada y comentando tipos de datos.

--- frontend/src/app/user/components/navbar-pyme/navbar-pyme.component.ts
+++ frontend/src/app/user/components/navbar-pyme/navbar-pyme.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../../core/services/auth/auth.service';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar-pyme',
@@ -8,21 +8,19 @@ import { AuthService } from '../../../core/services/auth/auth.service';
   styleUrls: ['./navbar-pyme.component.css']
 })
 export class NavbarPymeComponent implements OnInit {
-
-  nombreUsuario: string = this.authService.usuario.nombreUsuario;
+  userName: string = this.authService.usuario.nombreUsuario;
 
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
   ) { }
 
-
   ngOnInit(): void {
-    console.log(this.nombreUsuario)
+    // console.log(this.userName);
   }
 
-  logOut() {
-    this.authService.logOut()
-    this.router.navigate(['visitor'])
+  public logOut() {
+    this.authService.logOut();
+    this.router.navigate(['visitor']);
   }
 }
