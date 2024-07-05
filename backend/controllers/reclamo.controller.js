c5fa29946199df5c4568d0bb7e96cfce092b61e9
Sebastian Vidal <sebastianvr63@gmail.com>
2024-07-05 17:28:41 -0400

    feat: Nueva ruta y controlador para manejar reclamos archivados.

--- backend/controllers/reclamo.controller.js
+++ backend/controllers/reclamo.controller.js
@@ -134,6 +134,132 @@ const reclamosGetAll = async (req = request, res = response) => {
     }
 };
 
+const reclamosFinishedGetAll = async (req = request, res = response) => {
+    console.log('[reclamos] reclamosFinishedGetAll()');
+
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 20;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const baseFilter = {
+        estado: true,
+        mensajeAdmin: { [Op.ne]: null } // Verifica que mensajeAdmin no sea nulo o vacío
+    };
+
+    const additionalFilters = [];
+
+    if (req.query.nombre) {
+        additionalFilters.push({
+            [Op.or]: [
+                { '$Usuario.Pyme.nombrePyme$': { [Op.like]: `%${req.query.nombre.toLowerCase()}%` } },
+                { '$Publicacion.Usuario.Pyme.nombrePyme$': { [Op.like]: `%${req.query.nombre.toLowerCase()}%` } }
+            ]
+        });
+    }
+
+    if (req.query.fecha) {
+        const dateRegex = /^\d{2}-\d{2}-\d{4}$/;
+        if (dateRegex.test(req.query.fecha)) {
+            const fechaParts = req.query.fecha.split('-');
+            const day = parseInt(fechaParts[0], 10);
+            const month = parseInt(fechaParts[1], 10);
+            const year = parseInt(fechaParts[2], 10);
+            // console.log('Parsed Date:', { day, month, year });
+
+            additionalFilters.push({
+                [Op.and]: [
+                    Sequelize.where(Sequelize.fn('DAY', Sequelize.col('Reclamo.createdAt')), day),
+                    Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('Reclamo.createdAt')), month),
+                    Sequelize.where(Sequelize.fn('YEAR', Sequelize.col('Reclamo.createdAt')), year),
+                ],
+            });
+        }
+    }
+
+    const filter = {
+        ...baseFilter,
+        ...additionalFilters.length > 0 ? { [Op.and]: additionalFilters } : {},
+    };
+    // console.log({ filter });
+
+    try {
+        const { count, rows: reclamos } = await Reclamo.findAndCountAll({
+            where: filter,
+            limit: pageSize,
+            offset: (page - 1) * pageSize,
+            order: [['createdAt', 'DESC']],
+            include: [
+                {
+                    model: Usuario,
+                    as: 'Usuario',
+                    where: { estado: true },
+                    include: [
+                        {
+                            model: Pyme,
+                            as: 'Pyme',
+                            where: { estado: true },
+                        },
+                    ],
+                },
+                {
+                    model: Publicacion,
+                    as: 'Publicacion',
+                    where: { estado: true },
+                    include: [
+                        {
+                            model: Usuario,
+                            as: 'Usuario',
+                            where: { estado: true },
+                            attributes: ['nombreUsuario'],
+                            include: {
+                                model: Pyme,
+                                as: 'Pyme',
+                                where: { estado: true },
+                                attributes: ['nombrePyme'],
+                            },
+                        },
+                    ],
+                },
+            ],
+        });
+
+        if (!reclamos.length) {
+            if (additionalFilters.length > 0) {
+                return res.status(200).json({
+                    message: 'No se encontraron coincidencias para los filtros aplicados.',
+                    noSearchMatch: true,
+                    reclamos: []
+                });
+            } else {
+                return res.status(200).json({
+                    message: 'No se encontraron reclamos.',
+                    reclamos: [],
+                });
+            }
+        }
+
+        return res.status(200).json({
+            total: count,
+            totalPages: Math.ceil(count / pageSize),
+            currentPage: page,
+            pageSize,
+            reclamos,
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Error en el servidor, reclamosFinishedGetAll().',
+            error
+        });
+    }
+};
+
 const reclamoPost = async (req = request, res = response) => {
     console.log('[reclamos] reclamoPost()');
 
@@ -261,5 +387,6 @@ module.exports = {
     reclamoPost,
     reclamoPut,
     reclamoDelete,
-    reclamoUpdateAdminMessage
+    reclamoUpdateAdminMessage,
+    reclamosFinishedGetAll,
 };
--- backend/routes/reclamo.routes.js
+++ backend/routes/reclamo.routes.js
@@ -6,7 +6,8 @@ const { validarCampos } = require('../middlewares/validar-campos');
 const {
     reclamosGetAll,
     reclamoPost,
-    reclamoUpdateAdminMessage
+    reclamoUpdateAdminMessage,
+    reclamosFinishedGetAll
 } = require('../controllers/reclamo.controller');
 
 const router = Router();
@@ -43,11 +44,30 @@ router.get('/', [
         .withMessage('La fecha proporcionada no es válida'),
 ], reclamosGetAll)
 
+router.get('/finished', [
+    validarJWT,
+    query('page').optional().isInt({ min: 1 }).
+        withMessage('page debe ser un número entero mayor o igual a 1'),
+    query('pageSize').optional().isInt({ min: 1, max: 100 }).
+        withMessage('pageSize debe ser un número entero entre 1 y 100'),
+    query('nombre').optional().isString()
+        .withMessage('El campo "nombre" debe ser una cadena (string)'),
+    query('fecha').optional()
+        .custom((value) => {
+            const dateRegex = /^\d{2}-\d{2}-\d{4}$/;
+            if (!dateRegex.test(value)) {
+                throw new Error('La fecha no es válida. Debe estar en formato DD-MM-YYYY');
+            }
+            return true;
+        })
+        .withMessage('La fecha proporcionada no es válida'),
+], reclamosFinishedGetAll)
+
 router.put('/:id/admin-message/', [
     validarJWT,
     check('id', 'El ID es obligatorio').not().isEmpty(),
     check('mensajeAdmin', 'El mensaje del admin es obligatorio').not().isEmpty(),
     validarCampos,
 ], reclamoUpdateAdminMessage);
- 
+
 module.exports = router;
