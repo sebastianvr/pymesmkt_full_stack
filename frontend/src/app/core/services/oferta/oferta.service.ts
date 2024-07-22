715eaa0ccdfc3682e34f5f43a12fe7f17b8d7b43
Sebastian Vidal <sebastianvr63@gmail.com>
2024-07-22 01:16:09 -0400

    feat : Nuevo servicio getCompras.

--- frontend/src/app/core/services/oferta/oferta.service.ts
+++ frontend/src/app/core/services/oferta/oferta.service.ts
@@ -1,25 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
-import { Observable, Subject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfertaService {
-
-  private _refresh$ = new Subject<void>();
-  private url: string = environment.baseUrl
+  private url: string = environment.baseUrl;
 
   constructor(
     private http: HttpClient,
   ) { }
-
-  get refresh$() {
-    return this._refresh$;
-  }
 
   postOferta(oferta: any): Observable<any> {
     return this.http.post<any>(`${this.url}/api/oferta/`, oferta);
@@ -30,12 +23,7 @@ export class OfertaService {
   }
 
   deleteOferta(idOferta: string): Observable<any> {
-    return this.http.delete<any>(`${this.url}/api/oferta/${idOferta}`)
-      .pipe(
-        tap(() => {
-          this.refresh$.next()
-        })
-      );
+    return this.http.delete<any>(`${this.url}/api/oferta/${idOferta}`);
   }
 
   aceptaOferta(oferta: any): Observable<any> {
@@ -60,6 +48,16 @@ export class OfertaService {
       .join('&');
       
     const urlWithQuery = `${this.url}/api/oferta/created/${id}?${queryString}`;
+    return this.http.get<any>(urlWithQuery);
+  }
+
+  getCompras(idUsuario: string, filters: any): Observable<any> {
+    const queryParams = { ...filters };
+    const queryString = Object.keys(queryParams)
+      .map(key => `${key}=${encodeURIComponent(queryParams[key])}`)
+      .join('&');
+      
+    const urlWithQuery = `${this.url}/api/oferta/pagada/${idUsuario}?${queryString}`;
     return this.http.get<any>(urlWithQuery);
   }
 }
