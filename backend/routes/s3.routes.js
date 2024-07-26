fca92cd563494d35b577b369391026459da90e98
Sebastian Vidal <sebastianvr63@gmail.com>
2024-07-26 13:22:07 -0400

    feat: Renombrando archivo s3.routes, añadiendo middewares validateImageFile y enpoint para guardar archivos en sobre publicaciones.

--- backend/routes/s3.routes.js
+++ backend/routes/s3.routes.js
@@ -5,15 +5,18 @@ const multer = require('multer');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const validateFiles = require('../middlewares/files/validate-file');
-const { uploadUserImage, getProfileUserImage, postReportFiles } = require('../controllers/minio.controller');
+const { uploadUserImage, getProfileUserImage, postReportFiles, postPublicationFiles } = require('../controllers/s3.controller');
+const validateImageFile = require('../middlewares/files/validate-image');
 
 const router = Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-router.post('/uploadUserImage',
+router.post('/uploadUserImage', [
     upload.single('image'),
+    // validateImageFile,
+],
     uploadUserImage
 );
 
@@ -21,11 +24,17 @@ router.get('/profileImage', [
     validarJWT,
     query('image').notEmpty().withMessage('El parámetro image es obligatorio'),
     validarCampos,
+    validateImageFile
 ], getProfileUserImage);
 
 router.post('/reportFiles', [
     validarJWT,
     validateFiles,
 ], postReportFiles);
+
+router.post('/publication', [
+    validarJWT,
+    validateFiles
+], postPublicationFiles);
 
 module.exports = router;
