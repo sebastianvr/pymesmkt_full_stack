458d6466afe9baf879e4634b65c392b553fc26dd
Sebastian Vidal <sebastianvr63@gmail.com>
2024-08-07 15:10:43 -0400

    feat: Implementando la subida de archivos para un nuevo reclamo.

--- frontend/src/app/user/pages/purchases/components/report-problem-form/report-problem-form.component.ts
+++ frontend/src/app/user/pages/purchases/components/report-problem-form/report-problem-form.component.ts
@@ -1,63 +1,96 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { MessageService } from 'primeng/api';
-import { catchError, finalize, of } from 'rxjs';
+import JSZip from 'jszip';
+import { catchError, of } from 'rxjs';
 import { S3FilesService } from 'src/app/core/services/files/s3-files.service';
+import { MessageService } from 'src/app/core/services/message/message.service';
 import { ReclamoService } from 'src/app/core/services/reclamo/reclamo.service';
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-report-problem-form',
   templateUrl: './report-problem-form.component.html',
   styleUrls: ['./report-problem-form.component.css']
 })
-export class ReportProblemFormComponent implements OnInit {
+export class ReportProblemFormComponent {
 
   reportForm!: FormGroup;
   uploadedFiles: any[] = [];
   @Input() report!: any;
   isLoading: boolean = false;
 
+  maxTitleCharacters = 150;
+  maxDescriptionCharacters = 900;
+  remainingTitleCharacters!: number;
+  remainingDescriptionCharacters!: number;
+  fileNames!: string[];
+
+  get titleControl(): AbstractControl | null {
+    return this.reportForm.get('titulo');
+  }
+
+  get descriptionControl(): AbstractControl | null {
+    return this.reportForm.get('descripcion');
+  }
+
+  get archivosControl(): AbstractControl | null {
+    return this.reportForm.get('archivos');
+  }
+
   constructor(
     private fb: FormBuilder,
-    private modalService: NgbModal,
     private activeModal: NgbActiveModal,
     private reclamoService: ReclamoService,
     private messageService: MessageService,
     private s3FilesService: S3FilesService,
-
   ) {
-
     this.buildClaimForm();
-    this.setFormClaimExample();
+    // this.setFormReportExample();
   }
 
-  ngOnInit(): void {
-  }
+  private buildClaimForm() {
+    this.reportForm = this.fb.group({
+      titulo: [null, [
+        Validators.required,
+        Validators.maxLength(this.maxTitleCharacters)
+      ]],
+      descripcion: [null, [
+        Validators.required,
+        Validators.maxLength(this.maxDescriptionCharacters)
+      ]],
+      archivos: [[],
+      [Validators.required]
+      ],
+    });
 
-  private buildClaimForm(): FormGroup {
-    return this.reportForm = this.fb.group({
-      titulo: [null, [Validators.required]],
-      descripcion: [null, [Validators.required]],
-      archivos: [null, []],
+    this.remainingTitleCharacters = this.maxTitleCharacters;
+    this.remainingDescriptionCharacters = this.maxDescriptionCharacters;
+
+    this.reportForm.get('titulo')?.valueChanges.subscribe(value => {
+      this.remainingTitleCharacters = this.maxTitleCharacters - (value ? value.length : 0);
+    });
+
+    this.reportForm.get('descripcion')?.valueChanges.subscribe(value => {
+      this.remainingDescriptionCharacters = this.maxDescriptionCharacters - (value ? value.length : 0);
     });
   }
 
-  private setFormClaimExample() {
+  private setFormReportExample() {
     return this.reportForm.reset({
-      titulo: 'Tuve un problema con mis productos ',
-      descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sit quidem molestiae. Magnam sit quidem molestiae. Magnam sit quidem molestiae.',
-      archivos: 'archivo.zip',
+      titulo: 'Problemas con la compra de computadoras',
+      descripcion: 'He adquirido recientemente un conjunto de computadoras de su tienda, pero desafortunadamente he encontrado varios problemas con los productos recibidos. El primer problema es que una de las computadoras no enciende, a pesar de seguir todas las instrucciones del manual. Además, he notado que la pantalla de otra computadora tiene varios píxeles muertos, lo cual afecta considerablemente su usabilidad.\n\nAdicionalmente, algunas de las especificaciones mencionadas en la página del producto no coinciden con las características reales de las computadoras recibidas. Por ejemplo, se prometía un disco duro de 1TB, pero solo recibí 500GB. Este tipo de inconsistencias son muy preocupantes y espero que puedan ofrecer una solución adecuada a estos inconvenientes lo antes posible.',
+      archivos: null,
     });
   }
 
-  public sendReport() {
-    this.isLoading = true;
-    console.log('sendReport()');
-    console.log(this.report);
-    console.log(this.uploadedFiles);
+  public async sendReport() {
 
+    if (this.reportForm.invalid) {
+      this.reportForm.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading = true;
     const userId = this.report.Ofertum.UsuarioId;
     const compraId = this.report.id;
     const publicationId = this.report.PublicacionId;
@@ -65,60 +98,82 @@ export class ReportProblemFormComponent implements OnInit {
       return;
     }
 
-    // Subir los archivos a Minio
-    this.s3FilesService.postNewReportFiles(this.uploadedFiles).pipe(
-      catchError(error => {
-        console.error('Error al subir archivos a Minio:', error);
-        Swal.fire({
-          position: 'center',
-          icon: 'error',
-          title: 'Hubo un problema al subir los archivos',
-          showConfirmButton: true
+    try {
+      // Subir archivos a s3
+      let nameFile;
+      if (this.uploadedFiles.length > 0) {
+        nameFile = await this.zipAndUploadFiles();
+      }
+
+      // Crear el reclamo con la URL del archivo (si existe)
+      const newReport: any = {
+        titulo: this.reportForm.get('titulo')?.value,
+        mensaje: this.reportForm.get('descripcion')?.value,
+        PublicacionId: publicationId,
+        UsuarioId: userId,
+        CompraId: compraId
+      };
+
+      // Solo agregar el campo documentos si nameFile no es null
+      if (nameFile) {
+        newReport.documentos = nameFile;
+      }
+
+      this.reclamoService.postReclamo(newReport).pipe(
+        catchError(error => {
+          console.error('Error al crear el reclamo:', error);
+          this.messageService.showErrorMessage('Error al intentar crear el reclamo.');
+          this.isLoading = false;
+          return of(null);
+        })
+      ).subscribe(data => {
+        if (data.ok === true) {
+          this.messageService.showSuccessMessage('Reclamo enviado');
+          this.isLoading = false;
+          this.activeModal.close(data);
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      this.messageService.showErrorMessage('Error al subir archivos');
+      this.isLoading = false;
+    }
+  }
+
+  private async zipAndUploadFiles() {
+    const zip = new JSZip();
+    this.uploadedFiles.forEach(file => {
+      zip.file(file.name, file);
+    });
+
+    return new Promise(async (resolve, reject) => {
+      try {
+        const zipBlob = await zip.generateAsync({
+          type: 'blob',
+          compression: 'DEFLATE',
+          compressionOptions: { level: 6 }
         });
-        return of(null);
-      })
-    ).subscribe((res: any) => {
-      if (res && res.filepaths) {
-        const filepathsString = res.filepaths.join(', ');
-        const newReport = {
-          titulo: this.reportForm.get('titulo')?.value,
-          mensaje: this.reportForm.get('descripcion')?.value,
-          documentos: filepathsString,
-          PublicacionId: publicationId,
-          UsuarioId: userId,
-          CompraId: compraId 
-        };
+        const zipFile = new File([zipBlob], 'archivos.zip', { type: 'application/zip' });
 
-        console.log({ newReport });
-
-        // Guardar el reporte
-        this.reclamoService.postReclamo(newReport).pipe(
+        this.s3FilesService.postNewReportFiles(zipFile).pipe(
           catchError(error => {
-            console.error('Error al enviar el reclamo:', error);
-            Swal.fire({
-              position: 'center',
-              icon: 'error',
-              title: 'Hubo un problema al enviar el reclamo',
-              showConfirmButton: true
-            });
+            console.error('Error al subir archivos a s3:', error);
+            this.messageService.showErrorMessage('Error al intentar subir archivos');
+            reject(error);
             return of(null);
-          }),
-          finalize(() => this.isLoading = false)
-        ).subscribe(response => {
-          if (response) {
-            console.log({ response });
-            const reportId = response.id;
-            Swal.fire({
-              position: 'center',
-              icon: 'success',
-              title: 'Tu reclamo ha sido guardado!',
-              showConfirmButton: false,
-              timer: 3000
-            }).then(() => {
-              this.activeModal.close(reportId);
-            });
+          })
+        ).subscribe((response: any) => {
+          if (response && response.filepath) {
+            const filePath = response.filepath;
+            resolve(filePath);
+          } else {
+            reject('No se obtuvieron rutas de archivo');
           }
         });
+      } catch (error) {
+        console.error('Error al crear el archivo zip:', error);
+        this.messageService.showErrorMessage('Hubo un problema al comprimir los archivos');
+        reject(error);
       }
     });
   }
@@ -130,9 +185,31 @@ export class ReportProblemFormComponent implements OnInit {
 
   public onUpload(event: any) {
     const fileList = event.currentFiles;
-    for (let file of fileList) {
-      this.uploadedFiles.push(file);
+    fileList.map((file: File) => this.uploadedFiles.push(file));
+    this.updateFileNamesInForm();
+  }
+
+  public onRemove(event: any) {
+    const removedFile = event.file;
+    this.uploadedFiles = this.uploadedFiles.filter(file => file !== removedFile);
+    this.updateFileNamesInForm();
+  }
+
+  private updateFileNamesInForm() {
+    const arrayFileNames = this.uploadedFiles.map((file: File) => file.name);
+    this.reportForm.get('archivos')?.setValue(arrayFileNames);
+    this.validateFiles();
+  }
+
+  // ToDo: Mostrar en pantalla el error cuando se requieren archivos.
+  private validateFiles() {
+    const archivosControl = this.reportForm.get('archivos');
+    if (this.uploadedFiles.length === 0) {
+      archivosControl?.setErrors({ required: true });
+    } else {
+      archivosControl?.setErrors(null);
     }
+    archivosControl?.updateValueAndValidity(); // Forzar reevaluación del control
   }
 
   public closeModal() {
