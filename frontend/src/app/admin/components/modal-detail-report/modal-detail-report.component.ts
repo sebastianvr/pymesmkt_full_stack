1bd6571e3ec8bea5f965d7fa9189a27de3e603a3
Sebastian Vidal <sebastianvr63@gmail.com>
2024-08-07 14:17:05 -0400

    feat: Manejo desde el front, la descarga de archivos para un reclamo específico.

--- frontend/src/app/admin/components/modal-detail-report/modal-detail-report.component.ts
+++ frontend/src/app/admin/components/modal-detail-report/modal-detail-report.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ReclamoService } from 'src/app/core/services/reclamo/reclamo.service';
+import { ModalArchivingComponent } from '../modal-archiving/modal-archiving.component';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-modal-detail-report',
@@ -8,12 +11,47 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ModalDetailReportComponent implements OnInit {
   @Input() detailReport: any;
+  @ViewChild('downloadLink') downloadLink!: ElementRef;
+  isLoadingFile!: boolean;
 
   constructor(
     public activeModal: NgbActiveModal,
+    public reclamoService: ReclamoService,
+    private modalService: NgbModal,
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  public downloadFile(nameFile: string) {
+    this.isLoadingFile = true;
+
+    this.reclamoService.getUrlFile(nameFile).
+      pipe(catchError((error => {
+        this.isLoadingFile = false;
+        return of(error);
+      }))).
+      subscribe((data: any) => {
+        const link: HTMLAnchorElement = this.downloadLink.nativeElement;
+        link.href = data.url;
+        link.download = '';
+        link.click();
+        this.isLoadingFile = false;
+      });
   }
 
+  public async endModalReport(id: string) {
+    const modalRef = this.modalService.open(ModalArchivingComponent, {
+      ariaLabelledBy: 'modal-basic-title',
+      centered: true,
+      size: 'md',
+    });
+    modalRef.componentInstance.idReport = id;
+
+    try {
+      const resultModalArchiving = await modalRef.result;
+      this.activeModal.close(resultModalArchiving);
+    } catch (error) {
+      // console.error(error);
+    }
+  }
 }
